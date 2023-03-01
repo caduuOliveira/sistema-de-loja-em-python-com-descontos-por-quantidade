804e1d1148cc2206110ebaa066f866924ccf95b3
Carlos Eduardo <120653163+caduuOliveira@users.noreply.github.com>
2023-03-01 01:47:26 -0300

    Update questao_1.py

--- questao_1.py
+++ questao_1.py
@@ -2,7 +2,7 @@
 print('Bem Vindo a Loja do Carlos Eduardo Silva De Oliveira')#identificador
 valorP = int(input('Entre com o valor do produto! '))# aqui capturo o valorP = valor do produto
 valorQ = int(input('Entre com o valor da quantidade! '))# aqui capturo o valorQ = valor da quantidade de produtos
-if(valorQ >= 10) and (valorQ <= 99): # se valor da quantidade for maior igual a 10 e menor igua a 99, 5% de desconto
+if(valorQ >= 10) and (valorQ <= 99): # se valor da quantidade for maior igual a 10 e menor igual a 99, 5% de desconto
     valorSemDesconto = valorP * valorQ # valor sem desconto é o valor do produto multiplicado pelo valor da quantidade
     desconto = valorSemDesconto * 0.05 # na variavel desconto eu salvo o valor sem desconto multiplicado por 0.05
     print(f'o valor do seu produto sem desconto é de: {valorSemDesconto}.00 R$')
